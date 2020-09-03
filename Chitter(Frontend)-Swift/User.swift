2e83641b353bc15db5a28fdd120205adb8ee182f
Chris Eadie <chris.eadie@outlook.com>
2020-09-03 11:21:13 +0100

    Add user model for signup and login

---
+++ Chitter(Frontend)-Swift/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Chitter(Frontend)-Swift
+//
+//  Created by Chris Eadie on 03/09/2020.
+//  Copyright © 2020 Chris Eadie Designs. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    var handle: String
+    var password: String
+}
