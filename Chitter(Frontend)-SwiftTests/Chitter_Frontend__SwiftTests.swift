46bb263e982c673f3eeaefde3f812ecb22d9acd2
Chris Eadie <chris.eadie@outlook.com>
2020-09-03 10:49:50 +0100

    Initial Commit

---
+++ Chitter(Frontend)-SwiftTests/Chitter_Frontend__SwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  Chitter_Frontend__SwiftTests.swift
+//  Chitter(Frontend)-SwiftTests
+//
+//  Created by Chris Eadie on 03/09/2020.
+//  Copyright © 2020 Chris Eadie Designs. All rights reserved.
+//
+
+import XCTest
+@testable import Chitter_Frontend__Swift
+
+class Chitter_Frontend__SwiftTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
