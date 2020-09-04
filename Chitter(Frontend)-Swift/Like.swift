20fdd47dccd01174050e8adb1b5b4ed43d2be4d5
Chris Eadie <chris.eadie@outlook.com>
2020-09-04 11:40:22 +0100

    Add like model

---
+++ Chitter(Frontend)-Swift/Like.swift
@@ -0,0 +1,13 @@
+//
+//  Like.swift
+//  Chitter(Frontend)-Swift
+//
+//  Created by Chris Eadie on 04/09/2020.
+//  Copyright © 2020 Chris Eadie Designs. All rights reserved.
+//
+
+import Foundation
+
+class Like: Codeable {
+    var user: User
+}
